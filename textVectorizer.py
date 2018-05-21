a73723e7f0cc64ef57cea9cc34c109c8b77ed1e7
alexioso <alexbraksator@gmail.com>
2018-05-20 18:45:17 -0700

    hi

---
+++ textVectorizer.py
@@ -0,0 +1,59 @@
+#textVectoriuzer.py
+
+import pandas as pd
+import sys
+import os
+from collections import Counter
+
+
+
+def main():
+	
+	root_dir = sys.argv[1]
+
+	test = [dI for dI in os.listdir(root_dir + "/C50test") if os.path.isdir(os.path.join(root_dir+"/C50test",dI))]
+	train = [dI for dI in os.listdir(root_dir + "/C50test") if os.path.isdir(os.path.join(root_dir+"/C50train",dI))]
+
+	documents = []
+	labels = []
+	for name in test:
+		print(name)
+		dir_name = root_dir + "/C50test/" + name
+		files = os.listdir(dir_name)
+
+		for file in files:
+			file_name = dir_name + "/" + file
+			with open(file_name, 'r') as myfile:
+				document = myfile.read().replace('\n', '')
+				documents.append(document.lower().replace("[^A-Za-z\s]", "").split()) 
+				labels.append(name)
+
+	for name in train:
+		print(name)
+		dir_name = root_dir + "/C50train/" + name
+		files = os.listdir(dir_name)
+
+		for file in files:
+			file_name = dir_name + "/" + file
+			with open(file_name, 'r') as myfile:
+				document = myfile.read().replace('\n', '')
+				documents.append(document) 
+				labels.append(name)
+
+
+	print("1")
+	texts = pd.DataFrame({"text":documents, "label":labels},index = range(len(documents)))
+	print("2")
+
+	bag_of_words = texts.loc[:,"text"].apply(Counter)
+	print("3")
+	print(bag_of_words.head())
+	tf = pd.DataFrame(list(bag_of_words))
+	print(tf.head())
+	
+
+
+
+
+if __name__ == "__main__":
+	main()
