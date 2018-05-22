ff65f1cf95cf836819d19f3710d4d47ef0406bd0
Purplexionist <eggertwilliar@gmail.com>
2018-05-22 12:34:17 -0700

    small start

--- textVectorizer.py
+++ textVectorizer.py
@@ -4,12 +4,19 @@ import pandas as pd
 import sys
 import os
 from collections import Counter
+import porter as ps
 
-
+#use py textVectorizer.py root [TRUE (this is for porter)] [stopwords (or NULL)]
 
 def main():
-	
+	fileG = open("groundtruth.csv", "w")
+	porter = False
+	stopWord = None
 	root_dir = sys.argv[1]
+	if(sys.argv[2] == "TRUE"):
+		porter = True
+	if(sys.argv[3] != "NULL"):
+		stopWord = sys.argv[3]
 
 	test = [dI for dI in os.listdir(root_dir + "/C50test") if os.path.isdir(os.path.join(root_dir+"/C50test",dI))]
 	train = [dI for dI in os.listdir(root_dir + "/C50test") if os.path.isdir(os.path.join(root_dir+"/C50train",dI))]
@@ -20,8 +27,9 @@ def main():
 	for name in test:
 		dir_name = root_dir + "/C50test/" + name
 		files = os.listdir(dir_name)
-
+		print(test)
 		for file in files:
+			fileG.write(file +","+name+"\n")
 			file_name = dir_name + "/" + file
 			with open(file_name, 'r') as myfile:
 				document = myfile.read().replace('\n', '')
@@ -35,18 +43,20 @@ def main():
 		files = os.listdir(dir_name)
 
 		for file in files:
+			fileG.write(file +","+name+"\n")
 			file_name = dir_name + "/" + file
 			with open(file_name, 'r') as myfile:
 				document = myfile.read().replace('\n', '')
 				documents.append(document) 
 				labels.append(name)
 
-	
 	texts = pd.DataFrame({"text":documents, "label":labels},index = range(len(documents)))
 
 	bag_of_words = texts.loc[:,"text"].apply(Counter)
 
 	print(bag_of_words.head())
+	print(len(texts))
+	fileG.close()
 	
 	
 
