ee3b2739f7e403f8abbe03df777775ce13fc46be
Arshan Hashemi <arshan321@yahoo.com>
2018-05-24 21:47:28 -0700

    Adding code for knn-Authorship and evaluation

--- classifierEvaluation.py
+++ classifierEvaluation.py
@@ -1,5 +1,7 @@
 import sys
 import numpy as np
+from collections import defaultdict
+
 from knnAuthorship import KNN
 
 def main():
@@ -10,15 +12,62 @@ def main():
     predictionsPath = sys.argv[1]
     groundTruthPath = sys.argv[2]
 
-    labels = {}
+    actual = []
+    authors = set()
     with open(groundTruthPath, 'r') as labelsFile:
-        for id, line in enumerate(labelsFile.readlines()):
-            labels[id] = line.split(',')[1].strip()
+        for line in labelsFile.readlines():
+            actual.append(line.split(',')[1].strip())
+            authors.add(line.split(',')[1].strip())
 
-    confusion = np.zeros(50,50)
-    with open(predictionsPath, 'w') as predictionsFile:
-        predictionsFile.
+    assert(len(authors) == 50)
+    authors = list(authors)
 
+    predicted = []
+    confusion = np.zeros((len(authors), len(authors)))
+    with open(predictionsPath, 'r') as predictionsFile:
+        for line in predictionsFile.readlines():
+            predicted.append(line.strip())
+
+    hits = defaultdict(int)
+    strikes = defaultdict(int)
+    misses = defaultdict(int)
+
+    for i in range(len(predicted)):
+        if predicted[i] == actual[i]:
+            hits[predicted[i]] += 1
+        else:
+            strikes[predicted[i]] += 1
+            misses[actual[i]] += 1
+
+        p = authors.index(predicted[i])
+        a = authors.index(actual[i])
+        confusion[p][a] += 1
+
+    print('Confusion matrix')
+    for c in confusion:
+        for x in c:
+            print("{}".format(x), end=' ')
+        print()
+
+    correct = 0
+    incorrect = 0
+    for key in hits.keys():
+        print(key)
+        print('     Hits:', hits[key])
+        print('     Strikes:', strikes[key])
+        print('     Misses:', misses[key])
+        precision = hits[key]/float(hits[key]+strikes[key])
+        recall = hits[key]/float(hits[key]+misses[key])
+        print('     Precision:', precision)
+        print('     Recall:', recall)
+        print('     F-1:', (2 * precision*recall)/(precision+recall))
+        correct += hits[key]
+        incorrect += misses[key]
+
+    print()
+    print('Correct:', correct)
+    print('Incorrect:', incorrect)
+    print('Accuracy:', correct/float(incorrect+correct))
 
 
 if __name__ == '__main__':
